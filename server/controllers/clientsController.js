7ea813ad44e14e672d1d29dcf1b0c564f3421f0a
wahyuragil21 <why.yuragil@gmail.com>
2024-02-24 11:36:17 +0700

    Feat : arsitektur mvc

---
+++ server/controllers/clientsController.js
@@ -0,0 +1,39 @@
+const clients = require("../models/clients")
+
+module.exports = class clientController {
+    static async createClient(req, res) {
+        try {
+            const { name, email, phone, credit } = req.body
+            if (!name || !email || !phone || !credit) throw new Error('required')
+
+            if (credit < 0) throw new Error('credit should be greater than 0')
+
+            const client = await clients.findOne({ where: { email } })
+            if(client) throw new Error('client already exists')
+
+            const newClient = await clients.create(...req.body)
+            res.status(201).json(newClient)
+        } catch (error) {
+            res.status(500).json({message : 'internal server error'})
+        }
+    }
+
+    static async loginClient(req, res) {
+        try {
+            const { email, password } = req.body
+            if (!email || !password) throw new Error('required')
+
+            const client = await clients.findOne({ where: { email } })
+            if(!client) throw new Error('client not found')
+
+            const isMatch = await comparePassword(password, client.password)
+            if(!isMatch) throw new Error('wrong password')
+
+            const accessToken = generateToken({id : client.id})
+            res.status(200).json({accessToken})
+        }catch(error) {
+            res.status(500).json({message : 'internal server error'})
+        }
+    }
+
+}
